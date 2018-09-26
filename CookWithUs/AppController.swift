28615a7eb32f7f4489a53d048f6804598811fab3
meeraveena54 <meeraveena54@gmail.com>
2018-09-25 21:23:01 -0700

    FirebaseConfiguration

---
+++ CookWithUs/AppController.swift
@@ -0,0 +1,17 @@
+//
+//  AppController.swift
+//  CookWithUs
+//
+//  Created by Meera R on 9/25/18.
+//  Copyright © 2018 Meera. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AppController{
+    func firebaseConfiguration()
+    {
+        FirebaseApp.configure()
+    }
+}
